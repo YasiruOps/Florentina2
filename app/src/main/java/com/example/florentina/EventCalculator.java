2a63eb53672e130dba449c0d7ec32fad163e4371
YasiruOps <yasirunayanasaraopanayaka@gmail.com>
2021-10-03 23:26:59 +0530

    nice

--- app/src/main/java/com/example/florentina/EventCalculator.java
+++ app/src/main/java/com/example/florentina/EventCalculator.java
@@ -3,6 +3,10 @@ package com.example.florentina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class EventCalculator extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class EventCalculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_calculator);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(EventCalculator.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
+
     }
 }
